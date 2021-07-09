022317523ca99e98794a4f5a5605fdb3cb96cbb3
phuongxtran122 <57684647+phuongxtran122@users.noreply.github.com>
2021-07-09 18:20:20 -0400

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,27 @@
-﻿function reservationSendData(methodPath, isReserve) {
-    $.post(methodPath, {}).done(function (data) {
-        if (data) {
+function reservationSendData(methodPath) {
+    $.post(methodPath, {}).done(function (success) {
+        if (success) {
             window.location.reload();
         }
         else {
             alert("Failed to complete your " + (isReserve)?"reservation":"cancelation");
         }
+    })
+}
+
+function partnerFinder(methodPath) {
+    $.post(methodPath, {}).done(function (success) {
+        if (success) {
+            window.location.reload();
+        }
+        else {
+            alert("Failed to complete your PartnerFinder request. Please try again");
+        }
+    })
+}
+
+function CheckAptNumberExist(methodPath) {
+    $.post(methodPath, {}).done(function (data) {
+        return data;
     })
 }
